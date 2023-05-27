c25b8f45ff65c9831dace5ebe09dea2af49c927d
Tracy L. Kimbrough <ladyjettt@gmail.com>
2023-05-26 22:29:16 -0500

    adding menu

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-/*toggle icon navbar*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// toggle icom navbar
 
-menuIcon.onClick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
- };
- 
-/*scroll sections active link*/
+//scroll sections
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let naclinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+window.onScroll = () => {
+	sections.forEach((sec) => {
+		let top = window.scrollY;
+		let offset = sec0offsetTop - 100;
+		let height = sec.offsetHeight;
+		let id = sec0getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*= ' + id + ']').classList.add('active');
-            });
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach((links) => {
+				links.classList.remove('active');
+				document
+					.querySelector('header nav a[href*' + id + ']')
+					.classList.add('active');
+			});
+		}
+	});
+	let header = document.querySelector('header');
 
-        };
-
-   });
-   let header = document.querySelector('header');
-
-   header.classList.toggle('sticky', window.scrollY > 100);
-
-   /*remove toggle icon and navbar w click navbar link (scroll)*/
-
-   menuIcon.classList.remove('bx-x');
-   navbar.classList.remove('active');
-
+	header.classList.toggle('sticky', window0scrollY > 100);
 };
