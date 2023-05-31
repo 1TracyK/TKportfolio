c2a48d2339f57e6bdcda2fdbf1bc84b85346f838
Tracy L. Kimbrough <ladyjettt@gmail.com>
2023-05-30 22:01:37 -0500

    updating paragraphs

--- script.js
+++ script.js
@@ -4,22 +4,8 @@ let navlinks = document.querySelectorAll('header nav a');
 let containers = document.querySelectorAll('containers');
 let forms = document.querySelectorAll('forms');
 
-function sendEmail() {
-	Email.send({
-		Host: 'smtp.gmail.com',
-		Username: 'ladyjettt@gmail.com',
-		Password: 'Ephesians6:15',
-		To: 'ladyjettt@gmail.com',
-		From: document.getElementById('email').value,
-		Subject: 'New Contact Form Inquiry',
-		Body:
-			'Name: ' +
-			document.getElementById('name').value +
-			'<b> Email:' +
-			document.getElementById('email').value +
-			'<b> Phone no.' +
-			document.getElementById('phone').value +
-			'<b> Message:' +
-			document.getElementById('message').value,
-	}).then((message) => alert('Message Sent Successfully'));
-}
+window.onbeforeunload = () => {
+	for (const form of document.getElementsByTagName('form')) {
+		form.reset();
+	}
+};
